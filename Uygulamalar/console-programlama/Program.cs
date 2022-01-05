b0d6a6519b244214076f2310dfbc93766921fe1d
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-06 02:27:19 +0300

    console-programlama

---
+++ Uygulamalar/console-programlama/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hoşgeldiniz...");
+
+Console.WriteLine("Lütfen adınızı giriniz");
+String name = Console.ReadLine();
+
+Console.WriteLine("Lütfen soyadınızı giriniz");
+String surname =Console.ReadLine();
+
+//Console.WriteLine("Merhaba" + name , surname);
+
+Console.WriteLine("Merhaba"+" "+name+" "+surname);
