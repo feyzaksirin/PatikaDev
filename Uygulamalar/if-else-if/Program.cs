8c9da809d8a3d3e982e3d32c370b96b4a9f74d65
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-08 00:10:24 +0300

    if-else-if

---
+++ Uygulamalar/if-else-if/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Merhabaaaa");
+
+int time = DateTime.Now.Hour;
+
+if (time>=6 && time<11)
+  {
+        Console.WriteLine("Günaydın!!");
+  }
+else if (time<= 18)
+{
+    Console.WriteLine("İyi günler!!");
+}   
+else
+{
+     Console.WriteLine("İyi geceler!!");
+}
+
+ string sonuc = time<=18 ? "iyi günler!!" : "İyi geceler!!";
+
+ sonuc = time>=6 && time<11 ? "Günaydın!!" : time<=18 ? "iyi günler!!" : "İyi geceler!!";
+ Console.WriteLine(sonuc);
