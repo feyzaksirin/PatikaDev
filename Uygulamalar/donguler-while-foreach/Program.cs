7d42a72441a3ae4390071be99f4cd508ff6520ae
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-09 21:00:46 +0300

    donguler-while-foreach

---
+++ Uygulamalar/donguler-while-foreach/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Merhabaa");
+
+Console.WriteLine("While");
+//While
+Console.WriteLine("1 den başlıyarak consoledan girilen sayıya kadar(sayı dahil) ortalama hesaplama");
+//1 den başlıyarak consoledan girilen sayıya kadar(sayı dahil) ortalama hesaplama
+
+Console.WriteLine("Lütfen bir sayı giriniz");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+     toplam += sayac;
+     sayac ++;
+}
+Console.WriteLine(toplam/sayi);
+
+Console.WriteLine("a dan z ye kadar tüm harfleri console yazdır ");
+//a dan z ye kadar tüm harfleri console yazdır
+char character = 'a';
+while (character < 'z')
+{
+    Console.WriteLine(character);
+    character ++;
+}
+
+Console.WriteLine("Foreach");
+//foreach
+string[] arabalar = {"BMW","Mercedes","Audi","Ford",};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
