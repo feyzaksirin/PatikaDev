cbba0483ad88db3401b2872c29d0e5ea2e136e6a
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-09 20:08:29 +0300

    donguler-for-loop

---
+++ Uygulamalar/donguler-for-loop/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace donguler_for_loop
+{ 
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Merhabaa");
+
+// Ekranda girilen sayıya kadar olan tek sayıları yazdırır
+Console.WriteLine("Ekranda girilen sayıya kadar olan tek sayıları");
+
+Console.WriteLine("Lütfen bir sayı giriniz: ");
+int sayac =int.Parse(Console.ReadLine());
+for (int i = 1; i <= sayac; i++)
+{
+    if(i%2 == 1)
+        Console.WriteLine(i);    
+}
+
+// 1 ile 100 arasındaki tek ve çift sayıların kendi içlerinde toplamlarını ekrana yazdırır
+Console.WriteLine("1 ile 100 arasındaki tek ve çift sayıların kendi içlerinde toplamları");
+
+int tekToplam = 0;
+int ciftToplam = 0;
+for (int i = 1; i <= 1000; i++)
+{
+    if (i%2 == 1)
+        tekToplam += i; // tekToplam = tekToplam + i;
+
+    else
+        ciftToplam += i;// tekToplam = tekToplam + i;
+
+}
+Console.WriteLine("Tek Toplam: " + tekToplam);
+Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+// break, continue
+Console.WriteLine("break ve continue");
+
+for (int i = 1; i < 10; i++)
+{
+    if (i==4)   
+        break;
+     Console.WriteLine(i);   
+}
+
+for (int i = 1; i < 10; i++)
+{
+    if(i==4)
+        continue;
+     Console.WriteLine(i);   
+}
+
+        }
+       
+    
+
+
+
+    }      
+}
