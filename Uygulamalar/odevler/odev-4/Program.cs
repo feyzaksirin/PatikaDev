d89833d43a03232e6a568ed70faf2479c05bb89d
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-16 16:45:08 +0300

    odev-4

---
+++ Uygulamalar/odevler/odev-4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Lütfen bir cümle giriniz: ");
+            string cumle = Console.ReadLine();
+            string liste = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ" + "abcçdefgğhiıjklmnoöprsştuüvyz";
+            string[] kelime = cumle.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelime.Length);
+
+            int sayac = 0;
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if (liste.Contains(cumle[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine("Harf sayısı: {0}", sayac);
+            
