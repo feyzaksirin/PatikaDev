ca4523850288628fdefae26bc31e5a6bc110cf3b
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-22 15:25:48 +0300

    array-list

--- Uygulamalar/array-list/Program.cs
+++ Uygulamalar/array-list/Program.cs
@@ -10,22 +10,22 @@ namespace array_list
          // System.Collections namespace
 
          ArrayList liste = new ArrayList();
-         // liste.Add("Feyza");
-         // liste.Add(2);
-         // liste.Add(true);
-         // liste.Add('G');
+         liste.Add("Feyza");
+          liste.Add(2);
+          liste.Add(true);
+          liste.Add('G');
 
          //içerisindeki verilere erişim
-         // Console.WriteLine(liste[1]);
+         Console.WriteLine(liste[1]);
 
-         // foreach (var item in liste)
-         //  Console.WriteLine(item);
+          foreach (var item in liste)
+          Console.WriteLine(item);
 
          //AddRange
          Console.WriteLine("****Add Range****");
-         // string[] renkler = {"kırmızı","sarı","mavi"};
+         string[] renkler = {"kırmızı","sarı","mavi"};
          List<int> sayilar = new List<int>(){1,4,6,8,12,30,};
-         //liste.AddRange(renkler);
+         liste.AddRange(renkler);
          //liste.AddRange(sayılar);
 
          foreach (var item in liste)
@@ -54,7 +54,8 @@ namespace array_list
          liste.Clear();
 
          foreach (var item in liste)
-          Console.WriteLine(item);
+          Console.WriteLine(item); 
+
       } 
    }
 }
