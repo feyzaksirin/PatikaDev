7738a6ba9837c714bc6bddd4bd7e14a329195cfa
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-29 00:20:34 +0300

    static-sinif-ve-uyeler

--- Uygulamalar/static-sinif-ve-uyeler/Program.cs
+++ Uygulamalar/static-sinif-ve-uyeler/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+        Calisan calisan = new Calisan("Feyza","Akşirin","İK");
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+    }
+}
+
+class Calisan
+{
+    private static int calisanSayisi;
+    private static int CalisanSayisi { get => calisanSayisi; }
+    private string Isim;
+    private string Soyisim;
+    private string Departman;
+    static Calisan() {
+        calisanSayisi = 0;
+    }
+
+    public Calisan(string isim, string soyisim, string departman)
+    {
+        this.Isim = isim;
+        this.Soyisim = soyisim;
+        this.Departman = departman;
+        calisanSayisi ++;
+    }
+}
