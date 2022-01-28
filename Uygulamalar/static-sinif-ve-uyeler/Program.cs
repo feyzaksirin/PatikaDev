58745c9132c12df93fb5bd593f21c9be0b7946ba
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-01-29 00:20:44 +0300

    static-sinif-ve-uyeler

--- Uygulamalar/static-sinif-ve-uyeler/Program.cs
+++ Uygulamalar/static-sinif-ve-uyeler/Program.cs
@@ -6,13 +6,20 @@
 
         Calisan calisan = new Calisan("Feyza","Akşirin","İK");
         Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+        Calisan calisan1 = new Calisan("Gazi","Hataş","İK");
+        Calisan calisan2 = new Calisan("Hayal","Hataş","İK");
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+        Console.WriteLine("Toplama işlemi sonucu {0}", Islemler.Topla(100,200));
+        Console.WriteLine("Çıkarma işlemi sonucu: [0}", Islemler.Çıkar(400,50));
+
     }
 }
 
 class Calisan
 {
     private static int calisanSayisi;
-    private static int CalisanSayisi { get => calisanSayisi; }
+    public static int CalisanSayisi { get => calisanSayisi; }
     private string Isim;
     private string Soyisim;
     private string Departman;
@@ -26,5 +33,17 @@ class Calisan
         this.Soyisim = soyisim;
         this.Departman = departman;
         calisanSayisi ++;
+    }
+}
+
+static class Islemler
+{
+    public static long Topla (int sayi1, int sayi2 )
+    {
+        return sayi1+ sayi2;
+    }
+    public static long Çıkar (int sayi1, int sayi2)
+    {
+        return sayi1- sayi2;
     }
 }
