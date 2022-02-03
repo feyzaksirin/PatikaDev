07e5a59e40e7957680ad90dbd1803aa60b55df46
Feyza Aksirin <feyzaksirin08@gmail.com>
2022-02-03 18:06:09 +0300

    odev-3

---
+++ Uygulamalar/odevler-2/odev-3/Program.cs
@@ -0,0 +1,24 @@
+﻿List<char> sesliList = new List<char>();
+        string sesliChar = "aeıioöuü";
+        System.Console.WriteLine("Lütfen Bir Kelime Giriniz.");
+        string metin = Console.ReadLine().ToLower();
+        for (int i = 0; i < metin.Length; i++)
+        {
+            if (sesliChar.Contains(metin[i]))
+            {
+                  
+                sesliList.Add(metin[i]);
+            }
+
+        }
+        if (sesliList.Count > 0)
+        {
+            foreach (var item in sesliList)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("Girilen Cümlede Sesli Harf Bulunmamaktadır.");
+        }
